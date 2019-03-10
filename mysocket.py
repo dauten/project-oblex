150fb1fe506a315e574e29f84605e33f17b616cc
Dale Auten <dauten96@gmail.com>
2019-03-10 03:01:10 -0500

    ping WORKSgit stage *! (poorly)

--- mysocket.py
+++ mysocket.py
@@ -6,9 +6,19 @@ import json
 
 USERS = set()
 
+async def con(websocket, path):
+    async for message in websocket:
+        print("got a message")
+        #for every reply (aka ping)...
+        data = json.loads(message)
+        if data['action'] == 'ping':
+            #forward it to everyone
+            print("returning ping")
+        if USERS:       # asyncio.wait doesn't accept an empty list
+            await asyncio.wait([user.send(message) for user in USERS])
 
-async def time(websocket, path):
-    USERS.add(websocket)
+
+async def pro(websocket, path):
     while True:
         try:
             board = json.loads(open("board.json", "r").read())
@@ -19,23 +29,31 @@ async def time(websocket, path):
                 tokens.append([each["filename"][:-4], each["row"]-1, each["column"]-1, 1, 1 ])
             for each in large["terrain"]:
                 tokens.append([each["filename"][:-4], each["tlx"]-1, each["tlx"]-1, each["height"], each["width"] ])
-
+            print("sending message")
             tokens = str(tokens).replace("[", "").replace("]", "")
             await websocket.send(tokens)
-            async for message in websocket:
-                print("got a message")
-                #for every reply (aka ping)...
-                data = json.loads(message)
-                if data['action'] == 'ping':
-                    #forward it to everyone
-                    print("returning ping")
-                if USERS:       # asyncio.wait doesn't accept an empty list
-                    await asyncio.wait([user.send(message) for user in USERS])
-
         except Exception as e:
             print("Error reading File, continueing...\n\n")
 
         await asyncio.sleep(.5)
+
+
+async def time(websocket, path):
+    USERS.add(websocket)
+    con_task = asyncio.ensure_future(
+    con(websocket, path)
+    )
+    pro_task = asyncio.ensure_future(
+    pro(websocket, path)
+    )
+
+    done, pending = await asyncio.wait(
+    [con_task, pro_task],
+    return_when=asyncio.FIRST_COMPLETED
+    )
+    for task in pending:
+        task.cancel()
+
 
 start_server = websockets.serve(time, 'auten.space', 5678)
 
